cd76446245f0d3418d4106f79d206df1226473c1
Matthew Goldsmith <31556412+mgoldsmith1@users.noreply.github.com>
2018-08-31 07:39:20 -0400

    Update code-skeleton.sh

--- code-skeleton.sh
+++ code-skeleton.sh
@@ -3,34 +3,29 @@
 # This is just a code skeleton for now
 
 #Checking Version of ?
-if type -p java; then
+if type -p temp; then
     echo found java executable in PATH
-    _java=java
-elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
-    echo found java executable in JAVA_HOME     
-    _java="$JAVA_HOME/bin/java"
+    _temp=temp
+elif [[ -n "$?" ]] && [[ -x "$?/bin/?" ]];  then
+    echo found ? executable in ?_HOME     
+    _?="$?_HOME/bin/?"
 else
-    echo "no java"
+    echo "no ?"
 fi
 
-if [[ "$_java" ]]; then
-    version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
+if [[ "$_temp" ]]; then
+    version=$("$_temp" -version 2>&1 | awk -F '"' '/version/ {print $2}')
     echo version "$version"
     
     if [[ "$version" > "1.8" ]] && [[ "$version" < "1.9" ]]; then
          echo Your Version of ? is more than ? but less than ?
          echo No need to Upgrade... Skipping Installation Step
          echo ? has already been installed.
-         echo INSTALLED.
-	 #Running Application
-	 #echo Running Application... 
-	 #./run.sh
+         echo Complete.
+	 
     elif [[ "$version" > "1.9" ]]; then        
          echo Your Version of ? is greater than ?
-         
-	 #Running Application
-	 #echo Running Application...
-	 #./run.sh
+        
     else 
  	 echo Your Version of ? is less than ?
 	 echo Upgrading to ?...
@@ -38,10 +33,11 @@ if [[ "$_java" ]]; then
 	 #With Homebrew we will install Java 8
 	 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 	 brew update
-	# brew tap caskroom/cask
-	# brew cask install caskroom/versions/java8
+       # brew tap caskroom/cask
+       # brew cask install caskroom/versions/java8
 
          echo INSTALLED.
+	 echo Complete.
 	
     fi
 fi
